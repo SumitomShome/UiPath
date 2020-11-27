25dddc8bc141e22b6230a62396fa12c18ed48f67
SumitomShome <sumitomshome@gmail.com>
2020-11-28 02:14:41 +0530

    [Sumitom] Add: Method for employee payroll

---
+++ EmployeePayrollService/EmployeePayrollTemplate/EmployeeUiQuery.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE EmployeeUiDatabase;
+USE EmployeeUiDatabase;
+CREATE TABLE EmployeeUiTableName
+(
+EmpID varchar(10),
+EmpName varchar(50),
+EmpEmail varchar(30),
+Age varchar(10),
+EmpDept varchar(50),
+EmpSalary varchar(20),
+EmpBankName varchar(50),
+Active_Inactive varchar(25),
+Attachment varchar(100)
+);
+SELECT * FROM EmployeeUiTableName;
+DELETE FROM EmployeeUiTableName;
